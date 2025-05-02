5e3f2c0ba5902edefe3870f9a376cbfa2f3e3632
prince.obina <princeobina92@gmail.com>
2025-05-02 14:25:22 +0800

    Foundation: basic routing and navigations

--- mui-components/src/main.jsx
+++ mui-components/src/main.jsx
@@ -1,10 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import App from './App';
+import Layout from './layouts/dashboard';
+// import Layout from './layouts/dashboard';
+// import DashboardPage from './pages';
+// import OrdersPage from './pages/orders';
+
+const router = createBrowserRouter([
+  {
+    Component: App,
+    children: [
+      {
+        path: '/',
+        Component: Layout,
+        children: [
+          {
+            path: '',
+            // Component: DashboardPage,
+          },
+          {
+            path: 'orders',
+            // Component: OrdersPage,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
